3f02de659e385c668b86ecb814281b2a5346ada9
ArtemBlyshchyk <artem.bl31@gmail.com>
2022-11-11 12:09:44 +0200

    Solved first Task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBTN = document.querySelector('button[data-start]');
+console.log(startBTN);
+const stopBTN = document.querySelector('button[data-stop]');
+console.log(stopBTN);
+const bodyRef = document.querySelector('body');
+console.log(bodyRef);
 
+startBTN.addEventListener('click', onStartBtnClick);
+stopBTN.addEventListener('click', onStopBtnClick);
+
+let interval = null;
+
+function onStartBtnClick () {
+    startBTN.disabled = true;
+    stopBTN.disabled = false;
+
+    interval = setInterval(() => {
+        const bkgColor = getRandomHexColor();
+        bodyRef.style.background = bkgColor;
+    }, 1000);
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+
+  function onStopBtnClick () {
+    startBTN.disabled = false;
+    stopBTN.disabled = true;
+    clearInterval(interval);
+  }
