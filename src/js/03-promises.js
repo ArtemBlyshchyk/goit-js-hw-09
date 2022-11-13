5cce3206f9cebc01af8064c4272a7236b5259168
ArtemBlyshchyk <artem.bl31@gmail.com>
2022-11-13 21:04:17 +0200

    Solved 3-rd task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,53 @@
+import Notiflix from "notiflix";
+
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', startPromise);
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+    return new Promise((resolve, reject) => {
+     
+        if (shouldResolve) {
+          return resolve({ position, delay });
+        } else {
+          return reject({ position, delay });
+        }
+  })
+};
+
+function startPromise(e) {
+  e.preventDefault()
+  const inputForm = e.currentTarget;
+
+  let delayEl = +inputForm['delay'].value;
+  const stepEl = +inputForm['step'].value;
+  const amountEl = +inputForm['amount'].value;
+
+  let position = 0;
+  
+    for (let i = 1; i <= amountEl; i += 1) {
+      position = i;
+      
+      const firstDelay = delayEl;
+      let delay = delayEl += stepEl;
+
+      createPromise(position, firstDelay, delay)
+            
+        .then(({ position, delay }) => {
+            
+          setTimeout(() => {
+            Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          }, delay);
+        })
+    
+        .catch(({ position, delay }) => {
+            
+          setTimeout(() => {
+            Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          }, delay);
+        })
+    }
+};
